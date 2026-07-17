32b4d817e6a8f74bd0610390bc2ba8a2419faf04
Julio Herrera <Julio Herrera>
2026-07-17 02:46:53 -0600

    feat: Controlador de pedido para escuchar una peticion y luego procesar la compra

---
+++ src/main/java/com/GrooveShop/grooveshop/controllers/PedidoController.java
@@ -0,0 +1,35 @@
+package com.GrooveShop.grooveshop.controllers;
+
+import com.GrooveShop.grooveshop.entities.DetallePedido;
+import com.GrooveShop.grooveshop.entities.Pedido;
+import com.GrooveShop.grooveshop.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService pedidoService;
+
+    public static class PedidoRequest {
+        public String nombreCliente;
+        public String correoCliente;
+        public List<DetallePedido> detalles;
+    }
+
+    @PostMapping
+    public Pedido procesarCompra(@RequestBody PedidoRequest request) {
+        return pedidoService.crearPedido(
+                request.nombreCliente,
+                request.correoCliente,
+                request.detalles
+        );
+    }
+}
